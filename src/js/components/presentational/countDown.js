7fca0caba6078ddd11e60f6442b5d812860bbfd3
Kristian Alvarez Jörgensen <krijor@kth.se>
2018-10-01 15:36:40 +0200

    Add cute rabbits

--- src/js/components/presentational/countDown.js
+++ src/js/components/presentational/countDown.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import rabbits from './../../../images/rabbits.svg';
 
 const CountDown = () => {
   const targetDate = new Date('2020-11-11T19:00:00.00');
 
   return (
-    <div className="timer">
-      {targetDate.toUTCString()}
+    <div className="container">
+      <div className="timer">
+        {targetDate.toUTCString()}
+      </div>
+      <img
+        src={rabbits}
+        id="rabbits"
+      />
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,27 @@ module.exports = {
         use: {
           loader: "html-loader"
         }
+      },
+      {
+        test: /\.svg$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            mimetype: 'image/svg+xml',
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
+      },
+      {
+        test: /\.(png|jp(e*)g)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
       }
     ]
   },
